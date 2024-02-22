9452e4770b01facda3c5d3ddc8b2d028a49df3ed
Ajay Pratap Singh <160117701+ajaySinghBBD@users.noreply.github.com>
2024-02-22 11:43:45 +0000

    game_Added

--- onoff/src/App.js
+++ onoff/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+ 
+import {useState} from 'react'
+ 
 function App() {
+  const [flag, setFlage]=useState(0);
+  const onhandleClick=()=>{
+    if(flag) setFlage(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <button onClick={()=>{setFlage(1)}}>Turn On</button>
+        <a href="https://www.w3schools.com" >
+             <img border="0" alt="W3Schools" style={flag===1?{transform: 'rotate(0deg)' }: {transform: 'rotate(180deg)' }} src={flag===1 ? "https://www.w3schools.com/js/pic_bulbon.gif" : "https://www.w3schools.com/js/pic_bulboff.gif" } width="100" height="100" />
+       </a>
+     
+        <button onClick={()=>{setFlage(0)}}>Turn Off</button>
+    </>
   );
 }
-
-export default App;
+ 
+export default App
